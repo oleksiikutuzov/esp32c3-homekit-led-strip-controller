08bada85545d8ab174de8f4497ff0d6645ec3618
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2023-09-25 21:01:15 +0200

    Bump OTA version

--- src/main.cpp
+++ src/main.cpp
@@ -89,6 +89,39 @@ void setupWeb();
 
 char sNumber[18] = "11:11:11:11:11:11";
 
+unsigned long ota_progress_millis = 0;
+
+void onOTAStart()
+{
+	// Log when OTA has started
+	Serial.println("OTA update started!");
+	// <Add your own code here>
+}
+
+void onOTAProgress(size_t current, size_t final)
+{
+	// Log every 1 second
+	if (millis() - ota_progress_millis > 1000)
+	{
+		ota_progress_millis = millis();
+		Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+	}
+}
+
+void onOTAEnd(bool success)
+{
+	// Log when OTA has finished
+	if (success)
+	{
+		Serial.println("OTA update finished successfully!");
+	}
+	else
+	{
+		Serial.println("There was an error during OTA update!");
+	}
+	// <Add your own code here>
+}
+
 struct Pixel_Strand
 	: Service::LightBulb
 { // Addressable RGBW Pixel Strand of nPixel Pixels
@@ -360,6 +393,7 @@ void loop()
 {
 	homeSpan.poll();
 	server.handleClient();
+	ElegantOTA.loop();
 }
 
 ///////////////////////////////
@@ -368,6 +402,9 @@ void setupWeb()
 {
 	STRIP->ip_address.setString(WiFi.localIP().toString().c_str());
 	ElegantOTA.begin(&server); // Start ElegantOTA
+	ElegantOTA.onStart(onOTAStart);
+	ElegantOTA.onProgress(onOTAProgress);
+	ElegantOTA.onEnd(onOTAEnd);
 	server.begin();
 	LOG1("HTTP server started");
 } // setupWeb
